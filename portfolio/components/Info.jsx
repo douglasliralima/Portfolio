6cf61b199d4a3b475de8f4ee6737c90ffccd0bc8
douglasliralima <douglasliralima@gmail.com>
2022-03-26 11:27:34 -0300

    Info basic structure

--- portfolio/components/Info.jsx
+++ portfolio/components/Info.jsx
@@ -1,7 +1,28 @@
 export default function Info() {
     return <section>
-      <h2>
-        Info
-      </h2>
+        <h2>
+            Info
+        </h2>
+        <div>
+            <h3>
+                Hello Stranger! <br />
+                Born in 1997 in São Paulo, Brazil. I'm passionate about software engineer, meet new people,
+                the marvel avengers and as you can see, drinking coffe.
+            </h3>
+            <p>
+                Description - <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam finibus fermentum ipsum nec lobortis. Integer efficitur sit amet dolor in pretium. Vestibulum sed posuere sapien, sit amet blandit tellus. Praesent euismod facilisis massa, id varius mauris condimentum eu. Etiam ullamcorper purus purus, vitae posuere libero porttitor eu. Proin volutpat ligula quis turpis interdum, vel fringilla quam semper. Nulla sit amet fermentum magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum pretium facilisis.
+                <br />
+                Integer malesuada fermentum est, at tempor elit aliquam in. Aliquam rutrum lacus sem, vitae iaculis nunc finibus vel. Quisque efficitur nisl vitae iaculis convallis. Nullam venenatis ac neque ac volutpat. Donec diam mi, ultricies ut dictum ac, tempus eu dolor. Donec maximus velit nec imperdiet elementum. Nunc molestie in augue ac ultricies.
+                <br />
+                Ut porta bibendum porttitor. Praesent pharetra mattis mollis. Curabitur vel interdum nisl, eu condimentum dui. Donec tristique ultricies tortor nec iaculis. Sed dui urna, pretium vel lobortis a, congue vel velit. Etiam pretium magna diam, non porta nunc venenatis eget. Proin volutpat eget ligula at lobortis. Donec id pulvinar mauris. Vestibulum nec odio est.
+                <br />
+                Morbi quis dolor mattis, ultrices purus ac, volutpat erat. Cras sit amet nisi cursus justo fermentum rutrum. Curabitur aliquam porttitor tristique. Integer tellus nisl, pharetra id purus vitae, feugiat varius purus. Suspendisse potenti. Quisque facilisis magna velit. Ut ut cursus lectus. Praesent pellentesque diam ac lectus venenatis condimentum. Nullam justo nibh, volutpat at vehicula eu, ullamcorper blandit mauris. Praesent accumsan porttitor libero vel laoreet. Praesent magna ante, volutpat a risus eu, feugiat suscipit est.
+            </p>
+        </div>
+        <div>
+            <h3>Image of myself</h3>
+            <p>Desireble characteristics in the photo -  Me drinking coffe and my name in the right bottom</p>
+        </div>
     </section>
 }
