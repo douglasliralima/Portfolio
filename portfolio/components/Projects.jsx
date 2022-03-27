92e8bb62df1328472b848fad6ff99e6d680b3d0f
douglasliralima <douglasliralima@gmail.com>
2022-03-27 09:30:41 -0300

    Added initial project description

--- portfolio/components/Projects.jsx
+++ portfolio/components/Projects.jsx
@@ -9,13 +9,22 @@ export default function Projects() {
                 JewelML Recommendations
             </h3>
             <span>Aug 2021 - Present</span>
+            <p>
+                Contribution at a microfrontend solution of product recommendations to retail stores, using React, 
+                Nodejs and github action for automation, as well as improving and maintaining it's infraestructure in 
+                google cloud platform.
+            </p>
         </div>
 
         <div>
             <h3>
-                Syn4me
+                Synchro4me
             </h3>
             <span>Feb 2020 - Sep 2021</span>
+            <p>
+                Maintenance and development of new features of a Cloud Solution with React, nodejs and spring boot, 
+                which deal with the brazilian tax and tributary scenario.
+            </p>
         </div>
 
         <div>
@@ -23,6 +32,10 @@ export default function Projects() {
                 Inventory Management System
             </h3>
             <span>jan 2019 - Feb 2020</span>
+            <p>
+                Creation of a API REST with flask, a web framework for Python, capable of do selling forecast of products 
+                and an automatic inventory estimate.
+            </p>
         </div>
 
         <div>
@@ -30,6 +43,9 @@ export default function Projects() {
                 Dungeon Scrolls
             </h3>
             <span>jan 2018 - jul 2018</span>
+            <p>
+                Native mobile application of a Role Player data manager and monster generator for Android devices.
+            </p>
         </div>
 
     </section>
