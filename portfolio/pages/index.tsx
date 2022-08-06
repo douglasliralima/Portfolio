435fdb11b7b899f9b1c8c50a4c280c7a9a2580dc
douglasliralima <douglasliralima@gmail.com>
2022-08-06 17:45:58 -0300

    Structure of home page

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -2,42 +2,70 @@ import type { NextPage } from 'next'
 
 import Grid from '@mui/material/Grid';
 
-import Projects from '../components/Projects'
-import Info from '../components/Info'
 import Head from 'next/head'
+import { Box } from '@mui/material';
 
-import Typograph from "../styles/typograph.module.css"
+
+const Header: NextPage = () =>
+  <Grid className="Header" alignItems="flex-start" container item>
+    <Grid item xs={2} sx={{ textAlign: "center" }}>
+      Software Engineer
+    </Grid>
+    <Grid item xs={2} sx={{ textAlign: "center" }}>
+      FullStack
+    </Grid>
+    <Grid container item xs={8} direction="row" justifyContent="flex-end">
+      <Grid item xs={1}>
+        Home
+      </Grid>
+      <Grid item xs={1}>
+        Work
+      </Grid>
+      <Grid item xs={1}>
+        About
+      </Grid>
+      <Grid item xs={1}>
+        Blog
+      </Grid>
+    </Grid>
+
+  </Grid>
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div style={{ marginTop: "5px" }}>
       <Head>
         <title>Douglas Felizardo</title>
         <meta name="description" content="Portfolio created by Douglas Felizardo" />
-        <link rel="icon" href="/favicon.ico" /> 
-        <link href="https://fonts.googleapis.com/css2?family=Tiro+Telugu&display=swap" rel="stylesheet"></link> 
+        <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Tiro+Telugu&display=swap" rel="stylesheet"></link>
       </Head>
 
-      <Grid container spacing={2} sx={{marginTop: "5px"}}>
-          <Grid item xs={2} sx={{textAlign: "center"}}>
-            Software Engineer
+      <Grid container spacing={2} sx={{ height: "100%" }}>
+        <Header />
+
+        <Grid container item>
+          <Grid item sm={6} sx={{ marginLeft: "20px" }}>
+            Douglas<br />
+            Lima
           </Grid>
-          <Grid item xs={2} sx={{textAlign: "center"}}>
-            FullStack
+        </Grid>
+
+        <Grid className="footer" container item alignItems="flex-end">
+          <Grid item xs={1} sx={{ textAlign: "center" }}>
+            Brazil 97
           </Grid>
-        <Grid container item xs={8} direction="row" justifyContent="flex-end">
+          <Grid container item xs={11} spacing={2} direction="row" justifyContent="flex-end">
             <Grid item xs={1}>
-              Home
-            </Grid>
-            <Grid item xs={1}>
-              Work
+              Github
             </Grid>
             <Grid item xs={1}>
-              About
+              Linkedin
             </Grid>
-            <Grid item  xs={1}>
-              Blog
+            <Grid item xs={1}>
+              Gmail
             </Grid>
+          </Grid>
         </Grid>
       </Grid>
 
