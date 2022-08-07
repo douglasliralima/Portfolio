6f2f83502a0acb3d8a9b8e91dbd7c1c4430281dc
douglasliralima <douglasliralima@gmail.com>
2022-08-07 16:57:12 -0300

    Typography of name at homepage

---
+++ portfolio/asset/meraki/readme.txt
@@ -0,0 +1,19 @@
+We’re an Open Source type foundry, dedicated towards positive change. We curate all our free-to-edit typefaces for specific campaigns to allow activists everywhere to work together with a unified voice and a targeted aesthetic to promote the change the world needs.
+
+Whether you’re an experienced type designer or you’re still trying to move on from the typewriter; good typography is for all. Absolutely anyone, anywhere in the World is free to download, play with, break and reshape all our fonts.
+
+The only thing we ask for in return is change.
+
+https://www.extraboldfoundry.com/
+
+Meraki was created as a response to the staggering rise in the number of children who qualify for free school meals since the start of the Covid Pandemic.
+
+20% of children now fall within the threshold for this indicator of poverty in the UK and, time & time again, the government has proved reluctant to take the necessary actions to address the issue. Campaigners such as Marcus Rashford have made huge strides in the battle for food equality, but there remains a huge amount of work left to tackle the societal problems that have allowed the issue to rise to such disgraceful levels.
+
+All of the work that we do is completely not-for-profit.
+‍
+However, if you would like to show your appreciation for the work we do, or are just in a giving-kinda-mood, please consider donating to our curated charity-of-choice for each campaign.
+‍
+Imagine downloading a free typeface and helping to make a different in less that five minutes. Trust us; it's a good feeling.
+
+https://fareshare.org.uk/donate/
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -3,32 +3,33 @@ import type { NextPage } from 'next'
 import Grid from '@mui/material/Grid';
 
 import Head from 'next/head'
-import { Box } from '@mui/material';
 
+import styles from "./index.module.css"
 
 const Header: NextPage = () =>
-  <Grid className="Header" alignItems="flex-start" container item>
-    <Grid item xs={2} sx={{ textAlign: "center" }}>
-      Software Engineer
+  <Grid container item className="Header" >
+    <Grid alignItems="flex-start" container item>
+      <Grid item xs={2} sx={{ textAlign: "center" }}>
+        Software Engineer
+      </Grid>
+      <Grid item xs={2} sx={{ textAlign: "center" }}>
+        FullStack
+      </Grid>
+      <Grid container item xs={8} direction="row" justifyContent="flex-end">
+        <Grid item xs={1}>
+          Home
+        </Grid>
+        <Grid item xs={1}>
+          Work
+        </Grid>
+        <Grid item xs={1}>
+          About
+        </Grid>
+        <Grid item xs={1}>
+          Blog
+        </Grid>
+      </Grid>
     </Grid>
-    <Grid item xs={2} sx={{ textAlign: "center" }}>
-      FullStack
-    </Grid>
-    <Grid container item xs={8} direction="row" justifyContent="flex-end">
-      <Grid item xs={1}>
-        Home
-      </Grid>
-      <Grid item xs={1}>
-        Work
-      </Grid>
-      <Grid item xs={1}>
-        About
-      </Grid>
-      <Grid item xs={1}>
-        Blog
-      </Grid>
-    </Grid>
-
   </Grid>
 
 const Home: NextPage = () => {
@@ -38,16 +39,15 @@ const Home: NextPage = () => {
         <title>Douglas Felizardo</title>
         <meta name="description" content="Portfolio created by Douglas Felizardo" />
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Tiro+Telugu&display=swap" rel="stylesheet"></link>
       </Head>
 
       <Grid container spacing={2} sx={{ height: "100%" }}>
         <Header />
 
-        <Grid container item>
-          <Grid item sm={6} sx={{ marginLeft: "20px" }}>
-            Douglas<br />
-            Lima
+        <Grid container item alignItems="center">
+          <Grid item sx={{ marginLeft: "20px" }}>
+            <p className={styles.name1}>DOUGLAS LIMA</p>
+            <p className={styles.name2}>SOFTWARE ENGINEER</p>
           </Grid>
         </Grid>
 
